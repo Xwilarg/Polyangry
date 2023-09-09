1ed24cd9d500d4490c0762f2d16c909f6e251cd9
Xwilarg <xwilarg@protonmail.com>
2023-09-09 16:08:40 +0200

    Add main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RainbowJam2023.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
