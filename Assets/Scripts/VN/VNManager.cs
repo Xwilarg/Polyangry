bcc296f3f9e22e02d8ba5241d0a5d2304d9c2605
Xwilarg <xwilarg@protonmail.com>
2023-09-09 15:21:54 +0200

    Add VN engine

---
+++ Assets/Scripts/VN/VNManager.cs
@@ -0,0 +1,118 @@
+﻿using Ink.Runtime;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace RainbowJam2023.VN
+{
+    public class VNManager : MonoBehaviour
+    {
+        public static bool QuickRetry = false;
+        public static VNManager Instance { private set; get; }
+
+        [SerializeField]
+        private TextDisplay _display;
+
+        private Story _story;
+
+        [SerializeField]
+        private GameObject _container;
+
+        [SerializeField]
+        private TextAsset _intro;
+
+        [SerializeField]
+        private GameObject _namePanel;
+
+        [SerializeField]
+        private TMP_Text _nameText;
+
+        private bool _isSkipEnabled;
+        private float _skipTimer;
+        private float _skipTimerRef = .1f;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public bool IsPlayingStory => _container.activeInHierarchy;
+
+        private void Start()
+        {
+            ShowStory(_intro);
+        }
+
+        private void Update()
+        {
+            if (_isSkipEnabled)
+            {
+                _skipTimer -= Time.deltaTime;
+                if (_skipTimer < 0)
+                {
+                    _skipTimer = _skipTimerRef;
+                    DisplayNextDialogue();
+                }
+            }
+        }
+
+        private void ShowStory(TextAsset asset)
+        {
+            Debug.Log($"[STORY] Playing {asset.name}");
+            _story = new(asset.text);
+            _isSkipEnabled = false;
+            DisplayStory(_story.Continue());
+        }
+
+        private void DisplayStory(string text)
+        {
+            _container.SetActive(true);
+            _namePanel.SetActive(false);
+            foreach (var tag in _story.currentTags)
+            {
+                var s = tag.Split(' ');
+                var content = string.Join(' ', s.Skip(1));
+                switch (s[0])
+                {
+                    // TODO
+                }
+            }
+            _display.ToDisplay = text;
+        }
+
+        public void DisplayNextDialogue()
+        {
+            if (!_container.activeInHierarchy)
+            {
+                return;
+            }
+            if (!_display.IsDisplayDone)
+            {
+                // We are slowly displaying a text, force the whole display
+                _display.ForceDisplay();
+            }
+            else if (_story.canContinue && // There is text left to write
+                !_story.currentChoices.Any()) // We are not currently in a choice
+            {
+                DisplayStory(_story.Continue());
+            }
+            else if (!_story.canContinue && !_story.currentChoices.Any())
+            {
+                _container.SetActive(false);
+            }
+        }
+
+        public void ToggleSkip(bool value)
+            => _isSkipEnabled = value;
+
+        public void OnNextDialogue(InputAction.CallbackContext value)
+        {
+            if (value.performed)
+            {
+                DisplayNextDialogue();
+            }
+        }
+    }
+}
---
+++ Assets/Stories/Story.ink
@@ -0,0 +1,6 @@
+﻿-> intro
+== intro ==
+Hi I'm the developer of the game
+I'm writing this sample intro to test Ink integration
+Please whoever read this, edit this file properly
+-> DONE
