77627883226778187ba32d3367030e1db471cda3
Xwilarg <xwilarg@protonmail.com>
2023-09-12 22:50:52 +0200

    Add 'Interactible' object

---
+++ Assets/Scripts/Player/Interactible.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace RainbowJam2023.Player
+{
+    public class Interactible : MonoBehaviour
+    {
+        private UnityEvent _onAction;
+
+        public void InvokeAll()
+        {
+            _onAction.Invoke();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                collision.GetComponent<PlayerController>().ActionTarget = this;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                var pc = collision.GetComponent<PlayerController>();
+                if (pc.ActionTarget != null && pc.GetInstanceID() == GetInstanceID())
+                {
+                    pc.ActionTarget = null;
+                }
+            }
+        }
+    }
+}
